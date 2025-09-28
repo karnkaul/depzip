ad17889e2514035441fbdfc515ecb6cd331110eb
Karn Kaul <karnkaul@gmail.com>
2025-09-27 22:16:16 -0700

    Add tests

---
+++ tests/test_instance.cpp
@@ -0,0 +1,37 @@
+#include <depzip/instance.hpp>
+#include <depzip/panic.hpp>
+#include <klib/unit_test.hpp>
+#include <array>
+#include <filesystem>
+
+namespace {
+namespace fs = std::filesystem;
+
+TEST(instance) {
+	auto const pwd = fs::current_path();
+
+	auto instance = dz::create_instance();
+
+	auto const config = dz::Config{
+		.working_dir = "workspace",
+		.verbosity = dz::Verbosity::Verbose,
+	};
+
+	auto const packages = std::array{
+		dz::PackageInfo{.repo_uri = "karnkaul/depzip"},
+		dz::PackageInfo{.repo_uri = "glfw/glfw", .git_tag = "0.3.4", .remove_subpaths = {"docs", "examples"}},
+	};
+
+	try {
+		instance->vendor(packages, config);
+	} catch (dz::Panic const& panic) { ASSERT(false); }
+
+	fs::current_path(pwd);
+
+	EXPECT(fs::is_regular_file("workspace/src.zip"));
+	EXPECT(fs::is_directory("workspace/src/depzip"));
+	EXPECT(fs::is_directory("workspace/src/glfw"));
+
+	fs::remove_all("workspace");
+}
+} // namespace
